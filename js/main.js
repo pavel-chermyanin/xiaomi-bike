fcd7ed9d3f6af8c02fa2783da319c66b1f4b364e
Pavel <pavel_chermyanin@mail.ru>
2022-05-21 15:22:15 +0500

    laid out the sections slider, price, footer

--- js/main.js
+++ js/main.js
@@ -6,8 +6,22 @@ $(function() {
         autoplay: true,
         autoplaySpeed: 2000,
     })
+    $('.slider__items').slick({
+        arrows: false,
+        dots:true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
 
+    $('.menu, .bike__column').on('click', 'a', function(event) {
+        event.preventDefault();
 
+        const id = $(this).attr('href'),
+             top = $(id).offset().top
+
+        $('body, html').animate({scrollTop: top}, 1000)     
+    })
 
 
 
